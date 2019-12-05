209b373e32836b6d810f97dfa068d9cd5f5c780b
LGM-REFFO <ljm100696@163.com>
2019-12-05 17:00:50 +0800

    示例代码

---
+++ src/store/cart/state.js
@@ -0,0 +1,15 @@
+export default {
+    cartInfos:{
+        total_price:0,   //商品总价
+        total_amount:0,  //商品的数量
+        commitPrice:0,
+    },
+    cartList:[],   //购物车商品数组
+    curIndex:-1,  //当前商品操作的下标index
+    commitList:[], //购物车内商品购买的订单信息
+    // toBuyList:[],  //商品页的立即购买
+    commitAdress:[], //新增的地址
+    loginStatus:false,  //用户的登陆状态
+    count:0, //商品被选中的次数
+    toUrl:''
+ }
